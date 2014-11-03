87158a628216f52dc6549f5fa9a0771a70fd2a39
R0mai <r0maikx02b@gmail.com>
2014-11-03 21:26:45 +0100

    Reading in for tv

--- tv_optimization/main.cpp
+++ tv_optimization/main.cpp
@@ -1,6 +1,34 @@
 
+#include <vector>
 #include <iostream>
+
+struct Programme {
+    unsigned begin, length;
+};
+
+struct Tv {
+    std::string name;
+    std::vector<Programme> programs;
+};
 
 int main() {
 
+    typedef std::vector<Tv> Tvs;
+
+    Tvs tvs;
+
+    std::string name;
+    while (std::cin >> name) {
+        unsigned n;
+        std::cin >> n;
+
+        std::vector<Programme> programs(n);
+
+        for (unsigned i = 0; i < n; ++i) {
+            std::cin >> programs[i].begin >> programs[i].length;
+        }
+
+        tvs.push_back(Tv{name, programs});
+    }
+    std::cout << "out" << std::endl;
 }
