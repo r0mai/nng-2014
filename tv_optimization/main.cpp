5f70c9e3ef6ac06fbd7c0df7383b07e9408ad655
R0mai <r0maikx02b@gmail.com>
2014-11-04 23:38:24 +0100

    Add Bela's ingenuity

--- tv_optimization/main.cpp
+++ tv_optimization/main.cpp
@@ -4,8 +4,7 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
-
-#include <boost/optional.hpp>
+#include <algorithm>
 
 struct Programme {
     std::string tv;
@@ -13,84 +12,18 @@ struct Programme {
 };
 
 bool operator<(const Programme& lhs, const Programme& rhs) {
-    return lhs.begin < rhs.begin;
+    return lhs.begin + lhs.length < rhs.begin + rhs.length;
 }
-
-struct Mem {
-    std::vector<bool> hasIt;
-    unsigned coverage = 0;
-};
 
 typedef std::vector<Programme> Programs;
 
 Programs programs;
 
-Mem createMem() { Mem mem; mem.hasIt.resize(programs.size()); return mem; }
-
-unsigned getCoverage(const std::vector<bool>& hasIt) {
-    assert(hasIt.size() == programs.size());
-
-    unsigned coverage = 0;
-
-    for (unsigned i = 0; i < hasIt.size(); ++i) {
-        if (hasIt[i]) {
-            coverage += programs[i].length;
-        }
-    }
-
-    return coverage;
-}
-
-bool overlap(unsigned i, unsigned j) {
-    unsigned b1 = programs[i].begin;
-    unsigned e1 = programs[i].begin + programs[i].length;
-    unsigned b2 = programs[j].begin;
-    unsigned e2 = programs[j].begin + programs[j].length;
-    return e1 > b2 && e2 > b1;
-}
-
-Mem combine(const Mem& mem, unsigned n) {
-    assert(!mem.hasIt[n]);
-
-    Mem result = mem;
-
-    for (unsigned i = 0; i < programs.size(); ++i) {
-        if (!mem.hasIt[i]) {
-            continue;
-        }
-        if (overlap(i, n)) {
-            return result;
-        }
-    }
-
-    result.hasIt[n] = true;
-    result.coverage += programs[n].length;
-
-    assert(getCoverage(result.hasIt) == result.coverage);
-
-    return result;
-}
-
-std::vector<std::vector<boost::optional<Mem>>> memoize;
-
-Mem coverage(unsigned i, unsigned j) {
-    if (i == j) {
-        return createMem();
-    }
-    if (i+1 == j) {
-        Mem result = createMem();
-        result.hasIt[i] = true;
-        result.coverage = programs[i].length;
-        return result;
-    }
-
-    Mem max = createMem();
-    for (unsigned k = i; k < j; ++k) {
-    }
-
-}
-
 int main() {
+
+    typedef std::vector<Programme> Programs;
+
+    Programs programs;
 
     std::string name;
     while (std::cin >> name) {
@@ -105,5 +38,32 @@ int main() {
 
     std::sort(programs.begin(), programs.end());
 
+    /*
+     * Beware! Bela's ingenuity coming up!
+     */
 
+    // parcellaveghez  hozzarendelunk egy eddigi max osszeget + egy palyazok-szama vectort
+    std::map<unsigned, std::pair<unsigned, std::vector<unsigned>>, std::greater<unsigned>> s;
+
+    s[0] = std::make_pair(0 , std::vector<unsigned>());
+
+    for (unsigned i = 0; i < programs.size(); ++i)
+    {
+        unsigned a = programs[i].begin;
+        unsigned b = a + programs[i].length;
+        unsigned f = programs[i].length;
+        auto prev = s.upper_bound( a );
+        if ( s.begin()->second.first < prev->second.first + f )
+        {
+            std::vector<unsigned> cpy = prev->second.second;
+            cpy.push_back(i);
+            s[b] = std::make_pair( prev->second.first + f , cpy );
+        }
+    }
+    std::cout << s.begin()->second.first << std::endl;
+    for (auto x : s.begin()->second.second)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
 }
