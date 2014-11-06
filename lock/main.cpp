de6eaf25bb486929a35e9b9254bf2aaca34ce8c2
schaumb <schaumb@gmail.com>
2014-11-06 08:34:56 +0100

    change lock method, add lock3 result

--- lock/main.cpp
+++ lock/main.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 #include <vector>
-
+#include <unordered_map>
 #include <boost/multiprecision/cpp_int.hpp>
 
 typedef boost::multiprecision::cpp_int US;
+
 typedef unsigned long Index;
 typedef std::vector< US > VUS;
 typedef std::vector<VUS > MUS;
 
+typedef std::unordered_map< Index , std::pair< US , US > > UNS;
+typedef std::vector<  UNS > VUNS;
+typedef std::vector< VUNS > MUNS;
+
 MUS m;
 US res;
 VUS ns;
+US counter = 0;
+MUNS x;
 
-bool req( Index x , Index y , US sum )
-{
-    if( y >= ns.size() ) return false;
 
-    sum += m[x][y];
-
-    if( sum > res ) return false;
-    if( sum == res && y == ns.size() - 1 ) return true;
-    if( req( y + 1 , y + 1 , sum ) )
+bool doit( Index i , Index j , US max = res )
+{   
+    if( i == ns.size() )
     {
-        std::cout << "+ ";
         return true;
     }
-    if( req( x , y + 1 , sum ) )
+    for( auto n : x[i][j] )
     {
-        std::cout << "* ";
-        return true;
+        if( n.second.first <= max && max <= n.second.second )
+        {
+            if ( doit( j + 1 , n.first , max - m[i][j] ) )
+            {
+                std::cout << "multiply from " << i << " to " << j << " =  " << m[i][j]  << std::endl; 
+                return true;
+            }
+        }
     }
     return false;
 }
@@ -43,6 +50,9 @@ int main()
     ns.pop_back();
 
     m.resize( ns.size() , VUS( ns.size() ) );
+    x.resize( ns.size() , VUNS( ns.size() ) );
+
+
     for( Index i = 0; i < ns.size() ; ++i )
     {
         m[i][i] = ns[i];
@@ -50,12 +60,42 @@ int main()
         {
             m[i][j] = m[i][j-1] * ns[j];
         }
-        for( Index j = ns.size() - 1 ; j > 0 ; --j )
+        x[i][ns.size()-1].insert( std::make_pair ( ns.size() , std::make_pair( m[i][ns.size()-1] , m[i][ns.size()-1] ) ) );
+    }
+
+    for( Index j = ns.size() - 1 ; j > 0 ; )
+    {
+        --j;
+        for( Index i = 0 ; i <= j ; ++i )
         {
-            m[i][j] -= m[i][j-1];
+            for( Index sj = j + 1 ; sj < ns.size() ; ++sj )
+            {
+                auto it = std::begin( x[j+1][sj] );
+                US min = it->second.first;
+                US max = it->second.second;
+                for( ++it ; it != std::end( x[j+1][sj] ) ; ++it )
+                {
+                    if( it->second.first < min )
+                        min = it->second.first;
+                    if( it->second.second > max )
+                        max = it->second.second;
+
+                }
+                min += m[i][j];
+                max += m[i][j];
+                x[i][j].insert( std::make_pair ( sj , std::make_pair( min, max ) ) );
+                //std::cout << i <<" " << j << " " << sj <<" min:" << min << " max:" << max << std::endl;
+
+            }
         }
     }
-    req( 0 , 0 , 0 );
-    std::cout << " FORDITSD MEG OKET!" << std::endl;
+
+    for( Index j = ns.size() - 1 ; j > 0 ; )
+    {
+        --j;
+        std::cerr << j << std::endl;
+        if ( doit( 0 , j ) ) break;
+    }
+
     return 0;
 }
