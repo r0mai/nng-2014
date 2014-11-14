df32841dfda74651ee9bcb52fb2b3fc6af3311a7
schaumb <schaumb@gmail.com>
2014-11-14 19:29:38 +0100

    bloodbank fix

--- bloodbank/result/test_writer.cpp
+++ bloodbank/result/test_writer.cpp
@@ -16,14 +16,14 @@ int main(int argc , char** argv)
         std::size_t c; 
         do
         {
-            c = rand() % static_cast<std::size_t>(std::pow(10. , i % 5 + 2));
+            c = 100000;//rand() % static_cast<std::size_t>(std::pow(10. , i % 5 + 2));
         }
         while( c < 20 );	
         std::cout << i << " " << c << std::endl; 
         f << c << std::endl;
         std::size_t max_bad = c / 10;
         for( std::size_t i = 0; i < c ; ++i )
-            f << !(rand() % 10 || !max_bad || !--max_bad) << " ";
+            f << !(rand() % (c/10 - 1) || !max_bad || !--max_bad) << std::endl;
     }
     
 }
