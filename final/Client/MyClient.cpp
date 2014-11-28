2bb634133df89ee58344164ccb4fe4e82d3d6e9b
Mátyás Kuti <matyas.kuti@gmail.com>
2014-11-28 09:54:16 +0100

    Add half-done lookup table

--- final/Client/MyClient.cpp
+++ final/Client/MyClient.cpp
@@ -1,18 +1,58 @@
 #include "stdafx.h"
 #include "Client.h"
+#include <map>
 
 // sample
 
 const char *CommandList[]={ "check", "call", "bet" };
 
+enum Combination {AllTheSame, Full, Poker, Drill, DoublePair, Pair, Null};
+
+typedef std::vector<std::string> string_vector;
+typedef std::vector<std::vector<string_vector> > matrix;
+
+static const std::map<Combination, matrix> LOOKUP = {
+    {AllTheSame, 
+        {{{"bet", "bet", "bet", "bet" }}}
+    },
+    {Full,
+        {
+            {{"bet", "bet", "call", "call"}},
+            {{"bet", "bet", "bet", "call"}, {"bet", "bet", "call", "call"}},
+            {{"bet", "bet", "bet", "call"}, {"bet", "bet", "bet", "bet"}}
+        } 
+    },
+    {Poker,
+        {{{"bet", "bet", "bet", "call"}, {"bet", "bet", "bet", "bet"}}} 
+    },
+    {Drill,
+        {
+             
+        }
+    },
+    {DoublePair,
+        {
+
+        }
+    },
+    {Pair,
+        {
+
+        }
+    },
+    {Null,
+        {{{"check", "check", "check", "check"}}} 
+    }
+};
+
 class MYCLIENT : public CLIENT
 {
-public:
-	MYCLIENT();
-protected:
-	virtual std::string HandleServerResponse(std::vector<std::string> &ServerResponse);
-	virtual std::string GetPassword() { return std::string("4APCtg"); } // Nutellas Gereblye
-	virtual bool NeedDebugLog() { return true; }
+    public:
+    	MYCLIENT();
+    protected:
+    	virtual std::string HandleServerResponse(std::vector<std::string> &ServerResponse);
+    	virtual std::string GetPassword() { return std::string("4APCtg"); } // Nutellas Gereblye
+    	virtual bool NeedDebugLog() { return true; }
 };
 
 MYCLIENT::MYCLIENT()
